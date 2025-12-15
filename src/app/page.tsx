4c208397aa7648709d0a75ca948e7f99ca45abf4
Audrey Chen <Audgeviolin07@gmail.com>
2025-12-14 18:29:53 -0800

    general page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import FaultyTerminal from '../components/FaultyTerminal';
+import FeaturesSectionDemo from '../components/features-section-demo-1';
 
 export default function Home() {
   return (
-    <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
-      <FaultyTerminal
-        scale={1.5}
-        gridMul={[2, 1]}
-        digitSize={1.2}
-        timeScale={1}
-        pause={false}
-        scanlineIntensity={1}
-        glitchAmount={0}
-        flickerAmount={0}
-        noiseAmp={1}
-        chromaticAberration={0}
-        dither={0}
-        curvature={0}
-        tint="#ffffff"
-        mouseReact={true}
-        mouseStrength={0.5}
-        pageLoadAnimation={false}
-        brightness={1}
-      />
+    <div className="dark bg-black min-h-screen">
+      <FeaturesSectionDemo />
     </div>
   );
 }
